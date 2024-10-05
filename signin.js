3153cb6c240d3a0e20802517180a6a1e1e02d948
Vamshi2629 <147494918+Vamshi2629@users.noreply.github.com>
2024-10-05 20:35:45 +0530

    Update signin.js

--- signin.js
+++ signin.js
@@ -5,22 +5,41 @@ let loginPsd = document.getElementById("login-psd")
 loginForm.addEventListener("submit", handleLogin);
 let valid = false
 //  if(userDetails!=null){
-  function handleLogin(e) {
-    e.preventDefault()
+  // function handleLogin(e) {
+  //   e.preventDefault()
+  //   userDetails.map((item, i) => {
+  //     if ((item.username == loginName.value || item.email == loginName.value) && item.password == loginPsd.value) {
+  //       valid = true
+  //     }
+  
+  //   })
+  //   if (valid) {
+  //     // location.assign("http://127.0.0.1:5500/index1.html")
+  //     location.assign("index1.html")
+  //   }
+  //   else {
+  //     alert("invalid credentials")
+  //   }
+  // }
+function handleLogin(e) {
+  e.preventDefault()
+  let userDetails = JSON.parse(localStorage.getItem("userDetails"))
+  if (userDetails !== null) {
+    let valid = false
     userDetails.map((item, i) => {
       if ((item.username == loginName.value || item.email == loginName.value) && item.password == loginPsd.value) {
         valid = true
       }
-  
     })
     if (valid) {
-      // location.assign("http://127.0.0.1:5500/index1.html")
       location.assign("index1.html")
-    }
-    else {
+    } else {
       alert("invalid credentials")
     }
+  } else {
+    alert("No user details found in local storage")
   }
+}
 
 
 function passwordType() {
